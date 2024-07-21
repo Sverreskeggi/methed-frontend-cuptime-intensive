bf8b7f5ac6f98993dcd0cd47e12cef7ca28d5449
Sverreskeggi <alexanderpoilov85@gmail.com>
2024-07-21 17:46:51 +0300

    desktop done

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,28 @@
 'use strict';
 
-// // import Swiper JS
-// import Swiper from 'swiper';
-// // import Swiper styles
-// import 'swiper/css';
-
-// // const swiper = new Swiper(...);
+// const swiper = new Swiper('.swiper', {
+//     // Default parameters
+//     slidesPerView: 5,
+//     // spaceBetween: 1,
+//     // Responsive breakpoints
+//     breakpoints: {
+//       // when window width is >= 320px
+//       320: {
+//         slidesPerView: 2,
+//         // spaceBetween: 20
+//       },
+//       // when window width is >= 480px
+//       480: {
+//         slidesPerView: 3,
+//         // spaceBetween: 30
+//       },
+//       // when window width is >= 640px
+//       640: {
+//         slidesPerView: 4,
+//         // spaceBetween: 40
+//       }
+//     },
+//     autoplay: {
+//         delay: 200,
+//       },
+//   })
